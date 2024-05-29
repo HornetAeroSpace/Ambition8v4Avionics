06a5eca6869d8ea739642c462ba0d806c4b0d15c
mikeb <mberbach@csus.edu>
2024-05-29 16:19:36 -0700

    data update

--- test.cpp
+++ test.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 int main() {
+    /*
+    Order of data for data4.csv: min, sec, acceler x direction, accel y, acel z, gyro x, gyro y, gyro z
+    */
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
