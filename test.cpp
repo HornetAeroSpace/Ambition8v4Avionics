5011615887f9050938cd287e3627d25e33bc66d2
mikeb <mberbach@csus.edu>
2024-05-29 17:12:24 -0700

    good nuff to get goin

--- test.cpp
+++ test.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 int main() {
     /*
-    Order of data for data4.csv: min, sec, acceler x direction, accel y, acel z, gyro x, gyro y, gyro z
-    */
+    Order of data for data4.csv: min, sec, acceler x direction, accel y, acel z, gyro x, gyro y, gyro z- now irrel
+    imu min, sec, accel in xyz, sep 0 col, gyro x, others not reading?yz
+    4388 after is clean?    */
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
